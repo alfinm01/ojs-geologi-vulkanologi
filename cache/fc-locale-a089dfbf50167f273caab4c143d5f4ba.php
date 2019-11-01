872f8a5be6b35eca18a8db153645d383810d5f01
alfinm01 <arrestral@gmail.com>
2019-11-02 04:19:13 +0700

    Add cache

---
+++ cache/fc-locale-a089dfbf50167f273caab4c143d5f4ba.php
@@ -0,0 +1,8 @@
+<?php return array (
+  'plugins.importexport.doaj.displayName' => 'Plugin Ekspor DOAJ',
+  'plugins.importexport.doaj.description' => 'Ekspor Jurnal untuk DOAJ dan sediakan informasi jurnal untuk pencantuman',
+  'plugins.importexport.doaj.export.contact' => 'Kontak DOAJ untuk pencantuman',
+  'plugins.importexport.doaj.cliUsage' => 'Kegunaan:
+{$scriptName} {$pluginName} export [xmlFileName] [journal_path] articles objectId1 [objectId2] ...
+',
+); ?>
---
+++ cache/fc-locale-c5e44c44a95c0f691f170c7d7aadc08f.php
@@ -0,0 +1,8 @@
+<?php return array (
+  'plugins.importexport.crossref.displayName' => 'Plugin Ekspor CrossRef XML',
+  'plugins.importexport.crossref.description' => '> ekspor metadata artikel di format CrossRef XML.',
+  'plugins.importexport.crossref.cliUsage' => 'Kegunaan:
+{$scriptName} {$pluginName} export [xmlFileName] [journal_path] articles objectId1 [objectId2] ...
+{$scriptName} {$pluginName} register [journal_path] articles objectId1 [objectId2] ...
+',
+); ?>
